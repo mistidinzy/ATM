ff5c9093014a1e3d4d1de36690f23116bac1348d
Misti <mistidinzy@gmail.com>
2022-12-12 09:28:52 -0600

    starting log method

--- ATM.Tests/ATMTests.cs
+++ ATM.Tests/ATMTests.cs
@@ -3,98 +3,91 @@ using Xunit;
 
 namespace ATM.Tests
 {
-    public class BankAccountTests
+  public class BankAccountTests
+  {
+    [Fact]
+    public void GetBalance_Returns_Zero_Balance_For_New_Account()
     {
-        //[Fact]
-        //public void Test1()
-        //{
-        //    int zero = 1 - 2;
-        //    int num = 7 / zero;
-        //}
+      // Arrange
+      BankAccount bankaccount = new BankAccount();
 
-        [Fact]
-        public void GetBalance_Returns_Zero_Balance_For_New_Account()
-        {
-            // Arrange
-            BankAccount bankaccount = new BankAccount();
+      // Act
+      decimal result = bankaccount.GetBalance();
 
-            // Act
-            decimal result = bankaccount.GetBalance();
+      // Assert
+      Assert.Equal(0, result);
+    }
 
-            // Assert
-            Assert.Equal(0, result);
-        }
+    [Fact]
+    public void Deposit_Adds_To_Balance()
+    {
+      // Arrange
+      BankAccount bankaccount = new BankAccount();
 
-        [Fact]
-        public void Deposit_Adds_To_Balance()
-        {
-            // Arrange
-            BankAccount bankaccount = new BankAccount();
+      // Act
+      bankaccount.Deposit(30);
 
-            // Act
-            bankaccount.Deposit(30);
+      // Assert
+      decimal newBalance = bankaccount.GetBalance();
+      Assert.Equal(30, newBalance);
+    }
 
-            // Assert
-            decimal newBalance = bankaccount.GetBalance();
-            Assert.Equal(30, newBalance);
-        }
+    [Fact]
+    public void Deposit_Adds_Correct_Amount()
+    {
+      // Arrange
+      BankAccount bankaccount = new BankAccount();
 
-        [Fact]
-        public void Deposit_Adds_Correct_Amount()
-        {
-            // Arrange
-            BankAccount bankaccount = new BankAccount();
+      // Act
+      bankaccount.Deposit(30);
+      bankaccount.Deposit(50);
 
-            // Act
-            bankaccount.Deposit(30);
-            bankaccount.Deposit(50);
+      // Assert
+      decimal newBalance = bankaccount.GetBalance();
+      Assert.Equal(80, newBalance);
+    }
 
-            // Assert
-            decimal newBalance = bankaccount.GetBalance();
-            Assert.Equal(80, newBalance);
-        }
+    [Fact]
+    public void Throws_If_Negative_Deposit()
+    {
+      // Arrange
+      BankAccount bankaccount = new BankAccount();
 
-        [Fact]
-        public void Throws_If_Negative_Deposit()
-        {
-            // Arrange
-            BankAccount bankaccount = new BankAccount();
+      // Assert
+      Assert.Throws<ArgumentOutOfRangeException>(() =>
+      {
+        // Act
+        bankaccount.Deposit(-1);
+      });
+    }
 
-            // Assert
-            Assert.Throws<ArgumentOutOfRangeException>(() =>
-            {
-                // Act
-                bankaccount.Deposit(-1);
-            });
-        }
+    [Fact]
+    public void Withdraw_Subtracts_From_Balance()
+    {
+      //Arrange
+      BankAccount bankaccount = new BankAccount();
+      bankaccount.Deposit(3);
+      //Act
+      bankaccount.Withdraw(1);
 
-        [Fact]
-        public void Withdraw_Subtracts_From_Balance()
-        {
-            //Arrange
-            BankAccount bankaccount = new BankAccount();
-            bankaccount.Deposit(3);
-            //Act
-            bankaccount.Withdraw(1);
+      //Assert
+      decimal newBalance = bankaccount.GetBalance();
+      Assert.Equal(2, newBalance);
+    }
 
-            //Assert
-            decimal newBalance = bankaccount.GetBalance();
-            Assert.Equal(2, newBalance);
-        }
+    [Fact]
+    public void Withdraw_Amount_Is_More_Than_Balance()
+    {
+      // Arrange
+      BankAccount bankaccount = new BankAccount();
+      bankaccount.Deposit(3);
 
-        [Fact]
-        public void Withdraw_Amount_Is_More_Than_Balance()
-        {
-            // Arrange
-            BankAccount bankaccount = new BankAccount();
-            bankaccount.Deposit(3);
-
-            // Assert
-            Assert.Throws<ArgumentException>(() =>
-            {
-            //Act
-            bankaccount.Withdraw(5);
-            });
-        }
-    } 
+      // Assert
+      Assert.Throws<ArgumentException>(() =>
+      {
+        //Act
+        bankaccount.Withdraw(5);
+      });
+    }
+  }
 }
--- ATM/BankAccount.cs
+++ ATM/BankAccount.cs
@@ -1,41 +1,41 @@
 ﻿using System;
 namespace ATM
 {
-    public class BankAccount
+  public class BankAccount
+  {
+    private decimal balance;
+
+    public decimal GetBalance()
     {
-        private decimal balance;
-
-        public decimal GetBalance()
-        {
-            return balance;
-        }
-
-        public void Deposit(decimal depositMoney)
-        {
-            if (depositMoney < 0)
-            {
-                throw new ArgumentOutOfRangeException("Deposit amount cannot be negative.");
-            }
-
-            balance += depositMoney;
-        }
-
-        public void Withdraw(decimal amountToWithdraw)
-        {
-
-            if (amountToWithdraw > balance)
-            {
-                throw new ArgumentException("Withdrawal amount cannot exceed balance.");
-            }
-
-            if (amountToWithdraw < 0)
-            {
-                throw new ArgumentOutOfRangeException("You cannot withdraw a negative amount. Please enter a valid amount to withdraw.");
-            }
-
-            decimal newBalance = balance - amountToWithdraw;
-
-            balance -= amountToWithdraw;
-        }
+      return balance;
     }
+
+    public void Deposit(decimal depositMoney)
+    {
+      if (depositMoney < 0)
+      {
+        throw new ArgumentOutOfRangeException("Deposit amount cannot be negative.");
+      }
+
+      balance += depositMoney;
+    }
+
+    public void Withdraw(decimal amountToWithdraw)
+    {
+
+      if (amountToWithdraw > balance)
+      {
+        throw new ArgumentException("Withdrawal amount cannot exceed balance.");
+      }
+
+      if (amountToWithdraw < 0)
+      {
+        throw new ArgumentOutOfRangeException("You cannot withdraw a negative amount. Please enter a valid amount to withdraw.");
+      }
+
+      decimal newBalance = balance - amountToWithdraw;
+
+      balance -= amountToWithdraw;
+    }
+  }
 }
